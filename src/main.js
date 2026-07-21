7568b1cae49881e0a4bd1ff9877fb9168eab17c0
Daniel Viaña <spideycuentas2711@gmail.com>
2026-07-21 11:23:43 -0500

    Fix duplicate navbar import in main.js
    Removed duplicate import statement for navbar.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import "./styles/main.scss";
 import { navbar } from "../partials/navbar.js";
 initSidebar();
-
-import { navbar } from "../src/partials/navbar.js";
 const sidebarContainer = document.getElementById("header");
 if (sidebarContainer) {
     sidebarContainer.innerHTML = navbar();
